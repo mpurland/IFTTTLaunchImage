22876709f5c13e4209e92d370b9b97251da4f4e8
Matthew Purland <m.purland@gmail.com>
2016-05-10 13:52:13 -0700

    Added Carthage support

---
+++ IFTTTLaunchImage/IFTTTLaunchImage.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for IFTTTLaunchImage.
+FOUNDATION_EXPORT double IFTTTLaunchImageVersionNumber;
+
+//! Project version string for IFTTTLaunchImage.
+FOUNDATION_EXPORT const unsigned char IFTTTLaunchImageVersionString[];
+
+#import <IFTTTLaunchImage/IFTTTSplashView.h>
+#import <IFTTTLaunchImage/UIDevice+IFTTTLaunchImage.h>
+#import <IFTTTLaunchImage/UIImage+IFTTTLaunchImage.h>
+
